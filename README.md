f11a5a751eb720c1aa11d4c68ad8e63ac3d3a7ae
UnusualAbsurd <nottherealabsurdchad@gmail.com>
2022-04-11 12:34:55 +0700

    First release 😎

---
+++ README.md
@@ -0,0 +1,17 @@
+<div align="center">
+	<h1>Unchat Gateaway</h1>
+   <img src="https://cdn.discordapp.com/attachments/895567876285669376/962933100403392532/logo.png" alt="Logo"  />
+</div>
+
+## Unchat is a app where you can just chat with you friends.
+
+- This code is made using [Express](http://expressjs.com/)
+
+This thing is made by : [jadennns](https://jadennns.netlify.app/)
+
+# Environment Variables
+
+## Utilities
+
+- [socket.io](https://npmjs.com/package/socket.io)
+  Emiting socket events
---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const express = require("express");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: "*",
+  methods: ["GET", "POST"],
+});
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "There is nothing here..." });
+});
+
+io.on("connection", (socket) => {
+  socket.on("MESSAGE_CREATE", (payload) => {
+    io.sockets.emit("MESSAGE_CREATE", payload);
+  });
+});
+
+server.listen(5000, () => {
+  console.log(`Listening on PORT : ${5000}`);
+});
